37690519be15b6dcf3f4df79279dc3793dd6a31a
Xiaochao Dong (@damnever) <dxc.wolf@gmail.com>
2021-03-24 21:51:41 +0800

    fix: make gradient limit stable

--- README.md
+++ README.md
@@ -5,6 +5,6 @@
 A few of useful resilience patterns for Golang service.
 
 The circuitbreaker package is a logical copy from [Netflix Hystrix](https://github.com/Netflix/Hystrix/wiki/How-it-Works#CircuitBreaker).
-The concurrency package is modified from [Netflix concurrency-limits](https://github.com/Netflix/concurrency-limits) and [Envoy Proxy](https://www.envoyproxy.io/docs/envoy/latest/configuration/http/http_filters/adaptive_concurrency_filter).
+The concurrency package is modified from [Envoy Proxy](https://www.envoyproxy.io/docs/envoy/latest/configuration/http/http_filters/adaptive_concurrency_filter) and [Netflix concurrency-limits](https://github.com/Netflix/concurrency-limits).
 
 `concurrency.New(limit.NewGradientLimit(...))` is the first choice of server-side protection.
--- concurrency/limit/gradient.go
+++ concurrency/limit/gradient.go
@@ -12,36 +12,34 @@ import (
 
 // GradientOptions is the options for gradient limit.
 type GradientOptions struct {
-	// MinLimit is the initial limit and the minimal limit.
-	// The default value is 20.
+	// MinLimit is the allowed minimal limit.
+	// No need to change it in normal circumstances.
+	// The default value is 1.
 	MinLimit uint32
 	// MaxLimit is the allowed maximal limit.
-	// The default value is 300.
+	// The default value is 10000.
 	MaxLimit uint32
-	// RttToleranceFactor is the tolerance for the recorded minimal latency/rtt, it must equal or greater than 1.
-	// The default value is 1.5.
+	// RttToleranceFactor is the tolerance which will be applied to the measured minimal latency/rtt,
+	// a proper value maintains stability and maximizes the resource utilization.
+	// It must equal or greater than 1.
+	// The default value is 1.25.
 	RttToleranceFactor float64
-	// Smoothing makes the limit increasing/decreasing smoother, the bigger the steeper.
-	// The default value is 0.3.
-	Smoothing float64
 
+	// FIXME(damnever): expose those configurations.
 	// SampleWindow     time.Duration
 	// SamplePercentile float64
 	// MinRttWindow     time.Duration
 }
 
 func (opts *GradientOptions) withDefaults() {
-	if opts.MinLimit <= 0 {
-		opts.MinLimit = 20
+	if opts.MinLimit == 0 {
+		opts.MinLimit = 1
 	}
 	if opts.MaxLimit <= 0 {
-		opts.MaxLimit = 300
+		opts.MaxLimit = 10000
 	}
 	if opts.RttToleranceFactor == 0 {
-		opts.RttToleranceFactor = 1.5
-	}
-	if opts.Smoothing == 0 {
-		opts.Smoothing = 0.3
+		opts.RttToleranceFactor = 1.25
 	}
 }
 
@@ -50,10 +48,6 @@ func (opts GradientOptions) validate() error {
 	if opts.RttToleranceFactor < 1 {
 		return fmt.Errorf("concurrency/limit: RttToleranceFactor should equal or greater than 1: %f",
 			opts.RttToleranceFactor)
-	}
-	if opts.Smoothing < 0 || opts.Smoothing > 1 {
-		return fmt.Errorf("concurrency/limit: Smoothing should in range[0,1]: %f",
-			opts.Smoothing)
 	}
 	return nil
 }
@@ -64,40 +58,39 @@ type gradientLimit struct {
 	minLimit       uint32
 	maxLimit       uint32
 	rttTolerance   float64
-	smoothing      float64
-	countDown      uint32
-	maxCountDown   uint32
+
+	countDown    uint32
+	maxCountDown float64
 
 	summary *metric.Summary
 	min     *metric.WindowedMinimum
 }
 
-// NewGradientLimit creates a gradient algorithm based on the Gradient2 algorithm from Netflix
-// and the adaptive concurrency filter from Envoyproxy.
+// NewGradientLimit creates a gradient algorithm based on the adaptive concurrency filter from EnvoyProxy
+// and the Gradient2 algorithm from Netflix.
 // The empty options will be normalized to default values.
 //
-// The benchmark test shows that it is much more stable than the Gradient2 algorithm from Netflix.
 // Ref:
 //   - https://github.com/Netflix/concurrency-limits#gradient2
 //   - https://www.envoyproxy.io/docs/envoy/latest/configuration/http/http_filters/adaptive_concurrency_filter
 //   - https://github.com/envoyproxy/envoy/issues/7789
+//   - https://www.youtube.com/watch?v=CQvmSXlnyeQ
 func NewGradientLimit(opts GradientOptions) (Limit, error) {
 	(&opts).withDefaults()
 	if err := opts.validate(); err != nil {
 		return nil, err
 	}
-	maxCountDown := uint32(64) // FIXME(damnever): magic number.
+	maxCountDown := float64(125) // FIXME(damnever): magic number.
 	return &gradientLimit{
 		estimatedLimit: uint32(math.Ceil(float64(opts.MinLimit) * 1.1)), // Add a little buffer.
 		minLimit:       opts.MinLimit,
 		maxLimit:       opts.MaxLimit,
 		rttTolerance:   opts.RttToleranceFactor,
-		smoothing:      opts.Smoothing,
 		maxCountDown:   maxCountDown,
-		countDown:      uint32(calcCountDown(float64(opts.MinLimit), maxCountDown)),
+		countDown:      uint32(calcCountDown(float64(opts.MinLimit), 0, maxCountDown)),
 
 		summary: metric.NewSummary(0.95, 200*time.Millisecond, 40*time.Millisecond), // FIXME(damnever): magic numbers.
-		min:     metric.NewWindowedMinimum(60*time.Second, 1*time.Second),           // FIXME(damnever): magic numbers.
+		min:     metric.NewWindowedMinimum(90*time.Second, 2*time.Second),           // FIXME(damnever): magic numbers.
 	}, nil
 }
 
@@ -118,31 +111,36 @@ func (l *gradientLimit) Observe(startAt time.Time, rtt time.Duration, inflight u
 	if l.countDown--; l.countDown > 0 {
 		return l.estimatedLimit
 	}
+	if l.countDown == 0 {
+		l.countDown = 2 // In case of there is no samples.
+	}
 
 	sampleRtt := l.summary.Value()
+	minRtt := l.min.Value()
 	if sampleRtt < 0 {
 		return l.estimatedLimit
 	}
-	minRtt := l.min.Value()
 	if minRtt < 0 {
 		return l.estimatedLimit
 	}
 
 	gradient := math.Max(0.5, math.Min(2, l.rttTolerance*float64(minRtt)/float64(sampleRtt)))
-
 	estimatedLimit := float64(l.estimatedLimit)
 	newLimit := estimatedLimit * gradient
-	newLimit += math.Sqrt(newLimit) // Burst room.
-	newLimit = estimatedLimit*(1-l.smoothing) + newLimit*l.smoothing
+	if newLimit > estimatedLimit {
+		newLimit += math.Sqrt(newLimit) // Burst room.
+	}
+	// newLimit = estimatedLimit*(1-l.smoothing) + newLimit*l.smoothing?
 	newLimit = math.Max(float64(l.minLimit), math.Min(float64(l.maxLimit), newLimit))
 
 	// Apply jitter?
-	l.countDown = uint32(calcCountDown(newLimit, l.maxCountDown))
+	l.countDown = uint32(calcCountDown(newLimit, inflight, l.maxCountDown))
 
 	atomic.StoreUint32(&l.estimatedLimit, uint32(newLimit))
 	return l.estimatedLimit
 }
 
-func calcCountDown(limit float64, max uint32) float64 {
-	return math.Max(2, math.Min(float64(max), math.Round(limit/10))) // FIXME(damnever): magic numbers.
+func calcCountDown(limit float64, inflight uint32, max float64) float64 {
+	v := math.Min(float64(inflight)*0.8, math.Round(limit*0.6)) // FIXME(damnever): magic numbers.
+	return math.Round(math.Max(2, math.Min(max, v)))
 }
