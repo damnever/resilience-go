70a92292069b741cef3a6c85d7a661fc844f76a3
Xiaochao Dong (@damnever) <the.xcdong@gmail.com>
2021-12-02 21:06:01 +0800

    refactor!: reimplement the circuit breaker pattern
    BREAKING CHANGE:
    - make the number of probe calls configurable
    - refactor the state machine
    - almost all interface changed for better readability

--- circuitbreaker/circuitbreaker_test.go
+++ circuitbreaker/circuitbreaker_test.go
@@ -1,6 +1,7 @@
 package circuitbreaker
 
 import (
+	"fmt"
 	"sync"
 	"sync/atomic"
 	"testing"
@@ -9,138 +10,304 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-var _testconf = Config{
-	MetricsWindow:         time.Second,
-	SleepWindow:           500 * time.Millisecond,
-	TriggerThreshold:      10,
-	ErrorRateThreshold:    0.5,
-	SlowCallRateThreshold: 0.5,
-	SlowCallDuration:      10 * time.Second,
-	CoverPanic:            true,
-}
-
 func TestCircuitBreakerStateTransition(t *testing.T) {
-	cb := New(_testconf)
-	require.Equal(t, Close, cb.currentState())
+	testconf := Config{
+		MetricsSlidingWindow:      time.Second,
+		CooldownDuration:          500 * time.Millisecond,
+		AllowedProbeCalls:         2,
+		ObservationsThreshold:     10,
+		ErrorRateThreshold:        0.5,
+		SlowCallRateThreshold:     0.5,
+		SlowCallDurationThreshold: 10 * time.Second,
+	}
+	cb := New(testconf)
+	require.Equal(t, Closed, cb.State())
 
-	{ // CLOSE -> OPEN
-		for i := 0; i < _testconf.TriggerThreshold-1; i++ {
-			cb.trace(Close, time.Now(), false)
+	record := func(startAt time.Time, statusOk bool) {
+		state := cb.beforeCall()
+		if state.allowCall() {
+			cb.afterCall(state, startAt, statusOk)
 		}
-		require.Equal(t, Close, cb.currentState())
-		cb.trace(Close, time.Now(), true)
-		require.Equal(t, Open, cb.currentState())
 	}
-	{ // OPEN -> HALF-OPEN
-		require.Equal(t, Open, cb.currentState())
-		cb.setState(Open, time.Now().Add(-_testconf.SleepWindow+10*time.Millisecond))
-		time.Sleep(10 * time.Millisecond)
-		require.Equal(t, HalfOpen, cb.currentState())
-	}
-	{ // HALF-OPEN -> CLOSE
-		_, state, _ := cb.getState()
-		require.Equal(t, HalfOpen, state)
-		require.Equal(t, Open, cb.currentState())
-		cb.trace(HalfOpen, time.Now(), true)
-		require.Equal(t, Close, cb.currentState())
-	}
-	{ // CLOSE -> OPEN -> HALF-OPEN
-		require.Equal(t, Close, cb.currentState())
-		time.Sleep(time.Second / 2)
-		for i := 0; i < _testconf.TriggerThreshold; i++ {
-			cb.trace(Close, time.Now(), false)
-		}
-		require.Equal(t, Open, cb.currentState())
-		cb.setState(Open, time.Now().Add(-_testconf.SleepWindow+10*time.Millisecond))
-		time.Sleep(10 * time.Millisecond)
-		require.Equal(t, HalfOpen, cb.currentState())
-	}
-	{ // HALF-OPEN -> OPEN
-		_, state, _ := cb.getState()
-		require.Equal(t, HalfOpen, state)
-		require.Equal(t, Open, cb.currentState())
-		cb.trace(HalfOpen, time.Now(), false)
-		require.Equal(t, Open, cb.currentState())
-	}
-	{ // OPEN -> CLOSE
-		require.Equal(t, Open, cb.currentState())
-		for i := 0; i < _testconf.TriggerThreshold*2; i++ {
-			cb.trace(Close, time.Now(), true)
-		}
-		require.Equal(t, Close, cb.currentState())
+
+	for i, _tt := range []struct {
+		startFunc     func() // setState to start if startFunc is nil.
+		start         State
+		end           State
+		exec          func()
+		isInterrupted bool // isInterrupted after exec.
+	}{
+		{
+			start: Closed, end: Disabled,
+			exec: func() {
+				cb.Enable(false)
+				for i := 0; i < testconf.ObservationsThreshold; i++ {
+					record(time.Now(), false)
+				}
+			},
+			isInterrupted: false,
+		},
+		{
+			start: Disabled, end: Closed,
+			exec: func() {
+				cb.Enable(true)
+			},
+			isInterrupted: false,
+		},
+		{
+			start: Closed, end: ForceOpen,
+			exec: func() {
+				cb.ForceOpen()
+				for i := 0; i < testconf.ObservationsThreshold*2; i++ {
+					record(time.Now(), true)
+				}
+			},
+			isInterrupted: true,
+		},
+		{
+			start: ForceOpen, end: Closed,
+			exec: func() {
+				cb.Enable(true)
+			},
+			isInterrupted: false,
+		},
+		{
+			start: Closed, end: Closed,
+			exec: func() {
+				for i := 0; i < testconf.ObservationsThreshold-1; i++ {
+					record(time.Now(), false)
+				}
+			},
+			isInterrupted: false,
+		},
+		{ // ErrorRateThreshold
+			start: Closed, end: Open,
+			exec: func() {
+				for i := 0; i < testconf.ObservationsThreshold-1; i++ {
+					record(time.Now(), false)
+				}
+				record(time.Now(), true)
+			},
+			isInterrupted: true,
+		},
+		{ // SlowCallRateThreshold
+			start: Closed, end: Open,
+			exec: func() {
+				for i := 0; i < testconf.ObservationsThreshold-1; i++ {
+					record(time.Now().Add(-testconf.SlowCallDurationThreshold), true)
+				}
+				record(time.Now(), true)
+			},
+			isInterrupted: true,
+		},
+		{
+			start: Open, end: Open,
+			startFunc: func() {
+				cb.setState(Open, time.Now().Add(-testconf.CooldownDuration+10*time.Millisecond))
+			},
+			exec: func() {
+				time.Sleep(5 * time.Millisecond)
+			},
+			isInterrupted: true,
+		},
+		{
+			start: Open, end: HalfOpen,
+			startFunc: func() {
+				cb.setState(Open, time.Now().Add(-testconf.CooldownDuration+10*time.Millisecond))
+			},
+			exec: func() {
+				time.Sleep(11 * time.Millisecond)
+			},
+			isInterrupted: false,
+		},
+		{
+			start: Closed, end: HalfOpen,
+			exec: func() {
+				for i := 0; i < testconf.ObservationsThreshold-1; i++ {
+					record(time.Now(), false)
+				}
+				record(time.Now(), true)
+				cb.setState(Open, time.Now().Add(-testconf.CooldownDuration+10*time.Millisecond))
+				time.Sleep(11 * time.Millisecond)
+			},
+			isInterrupted: false,
+		},
+		{
+			start: HalfOpen, end: HalfOpen,
+			exec: func() {
+				for i := 0; i < testconf.AllowedProbeCalls; i++ {
+					cb.beforeCall().allowCall()
+				}
+			},
+			isInterrupted: true,
+		},
+		{
+			start: HalfOpen, end: Open,
+			exec: func() {
+				for i := 0; i < testconf.AllowedProbeCalls; i++ {
+					record(time.Now(), false)
+				}
+			},
+			isInterrupted: true,
+		},
+		{
+			start: HalfOpen, end: Closed,
+			exec: func() {
+				for i := 0; i < testconf.AllowedProbeCalls; i++ {
+					record(time.Now(), true)
+				}
+			},
+			isInterrupted: false,
+		},
+		{
+			start: Open, end: Closed,
+			startFunc: func() {
+				cb.setState(Open, time.Now().Add(-testconf.CooldownDuration+10*time.Millisecond))
+			},
+			exec: func() {
+				time.Sleep(11 * time.Millisecond)
+				for i := 0; i < testconf.AllowedProbeCalls; i++ {
+					record(time.Now(), true)
+				}
+			},
+			isInterrupted: false,
+		},
+	} {
+		tt := _tt
+		t.Run(fmt.Sprintf("#%02d#%s=>%s", i, tt.start, tt.end), func(t *testing.T) {
+			if tt.startFunc != nil {
+				tt.startFunc()
+				require.Equal(t, tt.start, cb.State())
+			} else {
+				cb.setState(tt.start, time.Now())
+			}
+			tt.exec()
+			state := cb.beforeCall()
+			require.Equal(t, tt.end, state.state)
+			require.Equal(t, tt.isInterrupted, !state.allowCall())
+		})
 	}
 }
 
-func TestCircuitBreakerConcurrently(t *testing.T) {
+func TestGuard(t *testing.T) {
+	cb := New(DefaultConfig())
+	g := cb.Guard()
+	require.True(t, g.Allow())
+	g.Observe(time.Now(), true)
+
+	// Check if any call passed.
+	g.state = nil
+	g.breaker = nil
+	for i := 0; i < 100; i++ {
+		require.False(t, g.Allow())
+		g.Observe(time.Now(), true)
+	}
+}
+
+func TestCircuitBreaker(t *testing.T) {
 	t.Parallel()
 
-	_testconf.TriggerThreshold = 20
-	_testconf.SleepWindow = 100 * time.Millisecond
-	cb := New(_testconf)
+	testconf := Config{
+		MetricsSlidingWindow:      time.Second,
+		CooldownDuration:          100 * time.Millisecond,
+		AllowedProbeCalls:         2,
+		ObservationsThreshold:     20,
+		ErrorRateThreshold:        0.5,
+		SlowCallRateThreshold:     0.5,
+		SlowCallDurationThreshold: 10 * time.Second,
+		OnStateChange: func(pre, current State) {
+			t.Logf("state: %s -> %s", pre, current)
+		},
+	}
+	cb := New(testconf)
 
 	// OPEN
 	wg := sync.WaitGroup{}
-	for i := 0; i < _testconf.TriggerThreshold; i++ {
+	for i := 0; i < testconf.ObservationsThreshold; i++ {
 		wg.Add(1)
 		go func(i int) {
 			defer wg.Done()
 
 			cb.Run(func() bool {
-				time.Sleep((_testconf.MetricsWindow / time.Duration(10)) * time.Duration(i%4))
+				time.Sleep((testconf.MetricsSlidingWindow / time.Duration(10)) * time.Duration(i%4))
 				return i%2 != 0
-			})
+			}, nil)
 		}(i)
 	}
 	wg.Wait()
-	require.True(t, cb.Circuit().IsInterrupted())
+	require.Equal(t, Open, cb.State())
 
 	// SLEEP: no request get through
-	for i := 0; i < _testconf.TriggerThreshold; i++ {
+	for i := 0; i < testconf.ObservationsThreshold; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			require.True(t, cb.Circuit().IsInterrupted())
+			require.False(t, cb.beforeCall().allowCall())
 		}()
 	}
 	wg.Wait()
-	time.Sleep(_testconf.SleepWindow)
+	time.Sleep(testconf.CooldownDuration)
 
-	// HALF-OPEN: only one request get through
-	var passed int32
-	for i := 1; i < _testconf.TriggerThreshold; i++ {
+	// HALF-OPEN: only AllowedProbeCalls request get through
+	require.Equal(t, HalfOpen, cb.State())
+	var probes int32
+	for i := 1; i < testconf.ObservationsThreshold; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			c := cb.Circuit()
-			if c.IsInterrupted() {
+			guard := cb.Guard()
+			if !guard.Allow() {
 				return
 			}
-			c.Observe(time.Now(), false)
-			atomic.AddInt32(&passed, 1)
+			guard.Observe(time.Now(), false)
+			atomic.AddInt32(&probes, 1)
 		}()
 	}
 	wg.Wait()
-	require.Equal(t, int32(1), passed)
+	require.Equal(t, int32(testconf.AllowedProbeCalls), probes)
 
 	{ // OPEN
-		c := cb.Circuit()
-		require.True(t, c.IsInterrupted())
+		require.Equal(t, Open, cb.State())
 	}
 
 	{
-		time.Sleep(_testconf.SleepWindow)
-		c := cb.Circuit()
-		require.False(t, c.IsInterrupted()) // HALF-OPEN
-		c.Observe(time.Now(), true)
-		require.False(t, c.IsInterrupted()) // CLOSE
+		time.Sleep(testconf.CooldownDuration)
+		for i := 0; i < testconf.AllowedProbeCalls; i++ {
+			guard := cb.Guard()
+			require.True(t, guard.Allow()) // HALF-OPEN
+			require.Equal(t, HalfOpen, cb.State(), i)
+			guard.Observe(time.Now(), true)
+		}
+		guard := cb.Guard()
+		require.True(t, guard.Allow()) // CLOSED
+		guard.Observe(time.Now(), true)
 	}
-	// CLOSE
-	for i := 0; i < _testconf.TriggerThreshold; i++ {
-		c := cb.Circuit()
-		require.False(t, c.IsInterrupted())
-		c.Observe(time.Now(), true)
+	// CLOSED
+	for i := 0; i < testconf.ObservationsThreshold; i++ {
+		guard := cb.Guard()
+		require.True(t, guard.Allow())
+		guard.Observe(time.Now(), true)
 	}
 
-	time.Sleep(_testconf.MetricsWindow)
-	require.False(t, cb.Circuit().IsInterrupted())
+	time.Sleep(testconf.MetricsSlidingWindow)
+	require.True(t, cb.Guard().Allow())
+}
+
+func BenchmarkCircuitBreaker(b *testing.B) {
+	cb := New(Config{
+		MetricsSlidingWindow:      500 * time.Millisecond,
+		CooldownDuration:          50 * time.Millisecond,
+		AllowedProbeCalls:         2,
+		ObservationsThreshold:     20,
+		ErrorRateThreshold:        0.1,
+		SlowCallRateThreshold:     0.2,
+		SlowCallDurationThreshold: 10 * time.Millisecond,
+	})
+	require.Equal(b, Closed, cb.State())
+
+	for i := 0; i < b.N; i++ {
+		cb.Run(func() bool {
+			return true
+		}, func() {})
+	}
 }
